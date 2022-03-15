8ed769c0041e5af5734599937436eafc476d3962
Alberto Noguchi <alberto2611@gmail.com>
2022-03-15 00:55:06 -0300

    first commit

---
+++ Intro/adopt_me/src/App.js
@@ -0,0 +1,29 @@
+// Components is like stamps
+
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h2", {}, "Luna"),
+        React.createElement("h3", {}, "Dog"),
+        React.createElement("h3", {}, "Havanese"),
+    ])
+}
+
+
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    // {} is an empty object and can be attributes
+   
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet),
+    
+  );
+};
+
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("root")
+);
