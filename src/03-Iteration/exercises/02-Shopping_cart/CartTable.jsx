630b84de7459e7fd1ba0c1801a251b95714452e1
Alberto Noguchi <alberto2611@gmail.com>
2024-10-01 17:27:46 -0300

    add: `iteration` exercise 02

---
+++ src/03-Iteration/exercises/02-Shopping_cart/CartTable.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import "./App.css";
+import "./App.jsx";
+
+function CartTable({ items }) {
+  // TODO: Map through “items”, creating 1 row
+  // per item.
+
+  return (
+    <table className="shopping-cart">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Title</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ imageSrc, imageAlt, title, price, id }) => (
+          <tr className="cart-row" key={id}>
+            <td>
+              <img className="product-thumb" src={imageSrc} alt={imageAlt} />
+            </td>
+            <td>{title}</td>
+            <td>${price}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default CartTable;
