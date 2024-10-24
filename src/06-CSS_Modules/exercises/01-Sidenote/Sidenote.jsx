ebb2354c9dd04191570817fd0f3c6a688c6f34e7
Alberto Noguchi <alberto2611@gmail.com>
2024-10-23 22:01:43 -0300

    add: styling in react exercise 01-sidenote solution

--- src/06-CSS_Modules/exercises/01-Sidenote/Sidenote.jsx
+++ src/06-CSS_Modules/exercises/01-Sidenote/Sidenote.jsx
@@ -2,8 +2,9 @@ import "./App.css";
 import styles from "./Sidenote.module.css";
 
 function Sidenote({ type, title, children }) {
+  const className = `${styles.wrapper} ${styles[type]}`;
   return (
-    <aside>
+    <aside className={className}>
       <h3 className={styles.title}>{title}</h3>
       <p>{children}</p>
     </aside>
