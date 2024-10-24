f2b887ce1261471a4ce5332f65c999efbcc045a4
Alberto Noguchi <alberto2611@gmail.com>
2024-10-23 22:37:03 -0300

    add: styling in react exercise 02-Movie rating animation solution

---
+++ src/06-CSS_Modules/exercises/02-Movie_rating_animation/Movie.jsx
@@ -0,0 +1,28 @@
+import styles from "./Movie.module.css";
+
+// Your mission:
+// Apply the 'glowingReview' CSS class to the
+// movie rating when it's 9 or higher.
+
+function Movie({ movie }) {
+  const ratingNumber = movie.rating;
+  return (
+    <article className={styles.movie}>
+      <div className={styles.thumbnailWrapper}>
+        <img alt="Movie poster" src={movie.posterSrc} />
+      </div>
+      <div className={styles.textWrapper}>
+        <h2>{movie.title}</h2>
+        <p className={styles.synopsis}>{movie.synopsis}</p>
+        <p>
+          <strong>Rating:</strong>{" "}
+          <span className={ratingNumber >= 9 && styles.glowingReview}>
+            {movie.rating}
+          </span>
+        </p>
+      </div>
+    </article>
+  );
+}
+
+export default Movie;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./06-CSS_Modules/exercises/01-Sidenote/App";
+import App from "./06-CSS_Modules/exercises/02-Movie_rating_animation/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
