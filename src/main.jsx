3f571d42452091a68b7e90becde3bf5f69ed4f67
Alberto Noguchi <alberto2611@gmail.com>
2024-10-24 21:41:01 -0300

    add: working with state event handlers exercise 01-click_the_ball_v2 solution

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./07-Event_Handlers/exercises/01-Click_the_ball/App";
+import App from "./07-Event_Handlers/exercises/02-Click_the_ball_v2/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
