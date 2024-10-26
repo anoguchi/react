2e1c7c6361c1f6473615b3937fe2e139bf9f9a5d
Alberto Noguchi <alberto2611@gmail.com>
2024-10-25 22:55:10 -0300

    add: the use state hook asynchronous updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./07-Event_Handlers/exercises/02-Click_the_ball_v2/App";
+import App from "./08-The_useState_hook/01-Asynchronous_updates/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
