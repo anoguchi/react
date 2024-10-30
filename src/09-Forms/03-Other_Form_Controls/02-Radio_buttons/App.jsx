54caedac2cae5df57f2b38116c37748d83057046
Alberto Noguchi <alberto2611@gmail.com>
2024-10-29 23:52:23 -0300

    add: forms other form controls

---
+++ src/09-Forms/03-Other_Form_Controls/02-Radio_buttons/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  const [value, setValue] = React.useState("no");
+
+  return (
+    <form>
+      <fieldset>
+        <legend>Do you agree?</legend>
+        <input
+          type="radio"
+          name="agreed-to-terms"
+          id="agreed-yes"
+          value="yes"
+          checked={value === "yes"}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+        />{" "}
+        <label htmlFor="agreed-yes">Yes</label>
+        <br />
+        <input
+          type="radio"
+          name="agreed-to-terms"
+          id="agreed-no"
+          value="no"
+          checked={value === "no"}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+        />{" "}
+        <label htmlFor="agreed-no">No</label>
+      </fieldset>
+    </form>
+  );
+}
+
+export default App;
