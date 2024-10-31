08fcaf181cbf9c8458d9cc5808f32f1f9f0d6fb6
Alberto Noguchi <alberto2611@gmail.com>
2024-10-30 21:40:00 -0300

    add: forms exercise 02-two_factor_authentication solution

---
+++ src/09-Forms/exercises/02-Two_Factor_Authentication/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import React from "react";
+
+const CORRECT_CODE = "123456";
+
+function TwoFactor() {
+  const [code, setCode] = React.useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    const isCorrect = code === CORRECT_CODE;
+    window.alert(isCorrect ? "Correct" : "Incorrect");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="auth-code">Enter authorization code:</label>
+      <div className="row">
+        <input
+          id="auth-code"
+          value={code}
+          type="text"
+          required={true}
+          maxLength={6}
+          onChange={(event) => {
+            setCode(event.target.value);
+          }}
+        />
+        <button>Validate</button>
+      </div>
+    </form>
+  );
+}
+
+export default TwoFactor;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./09-Forms/exercises/01-Controlled_Country_Picker/App";
+import App from "./09-Forms/exercises/02-Two_Factor_Authentication/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
