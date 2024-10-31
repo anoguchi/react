84396da4484a72285d2a3fa8ae512caf4a158aa9
Alberto Noguchi <alberto2611@gmail.com>
2024-10-31 00:51:31 -0300

    add: props vs state, state example

---
+++ src/10-Props_Vs_State/02-State/Button.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./App.css";
+import styles from "./Button.module.css";
+
+function Button({ variant, isEnabled, children }) {
+  return (
+    <button
+      className={`${styles.wrapper} ${styles[variant]}`}
+      disabled={!isEnabled}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./10-Props_Vs_State/01-Props/App";
+import App from "./10-Props_Vs_State/02-State/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
