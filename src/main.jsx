4ab9ba116309d3481b68ab0aa23122b26c86f949
Alberto Noguchi <alberto2611@gmail.com>
2024-10-30 22:19:28 -0300

    add: forms exercise 03-generative_art solution

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./09-Forms/exercises/02-Two_Factor_Authentication/App";
+import App from "./09-Forms/exercises/03-Generative_Art/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
