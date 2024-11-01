05b29521ca4e07fbae9068b9a3bae735f4935874
Alberto Noguchi <alberto2611@gmail.com>
2024-10-31 21:09:35 -0300

    add: complex state mutation bugs

---
+++ src/11-Complex_state/01-Mutation_Bugs/App.jsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  const [colors, setColors] = React.useState(["#FFD500", "#FF0040"]);
+
+  const colorStops = colors.join(", ");
+  const backgroundImage = `linear-gradient(${colorStops})`;
+
+  return (
+    <>
+      <div
+        className="gradient-preview"
+        style={{
+          backgroundImage,
+        }}
+      />
+
+      <form>
+        {colors.map((color, index) => {
+          const colorId = `color-${index}`;
+
+          return (
+            <div key={colorId} className="color-row">
+              <label htmlFor={colorId}>Color {index + 1}:</label>
+              <input
+                id={colorId}
+                type="color"
+                value={color}
+                onChange={(event) => {
+                  const nextColors = [...colors];
+                  nextColors[index] = event.target.value;
+
+                  setColors(nextColors);
+                }}
+              />
+            </div>
+          );
+        })}
+      </form>
+    </>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./10-Props_Vs_State/02-State/App";
+import App from "./11-Complex_state/01-Mutation_Bugs/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
