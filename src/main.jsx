01ecca209e0c786d62f1e74176406e6608ad005d
Alberto Noguchi <alberto2611@gmail.com>
2024-11-01 01:09:20 -0300

    add: complex state exercise 02-stretch_goal solution

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./11-Complex_state/exercises/01-Gradient_generator/App";
+import App from "./11-Complex_state/exercises/02-Stretch_Goal/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
