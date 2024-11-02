5d3ca1fde954d3e1e0e5ad42c0b7d8844edc5861
Alberto Noguchi <alberto2611@gmail.com>
2024-11-02 16:57:39 -0300

    add: dynamic key generation, key gotchas and lifting state up

---
+++ src/13-Lifting_State_Up/01-Search_fruits/SearchResults.jsx
@@ -0,0 +1,5 @@
+function SearchResults({ searchTerm }) {
+  return <p>You searched for: {searchTerm}.</p>;
+}
+
+export default SearchResults;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./12-Dynamic_Key_Generation/01-Stickers/App";
+import App from "./13-Lifting_State_Up/01-Search_fruits/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
