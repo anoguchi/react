cb5c8c3ec447ea26e84c247b4d8012665cdc79c4
Alberto Noguchi <alberto2611@gmail.com>
2024-11-07 00:14:02 -0300

    add: component instances

---
+++ src/14-Component_Instances/01-Random_number/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function RandomNumber() {
+  const [num, setNum] = React.useState(0);
+
+  return (
+    <button onClick={() => setNum(Math.random())}>Current number: {num}</button>
+  );
+}
+
+const root = createRoot(document.querySelector("#root"));
+root.render(
+  <>
+    <RandomNumber />
+  </>
+);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./13-Lifting_State_Up/exercises/02-Shopping_List/App";
+import App from "./14-Component_Instances/02-Toggle_Footer/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
